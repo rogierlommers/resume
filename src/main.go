fbfa88439cde77befa90b78f9b64a3a338cb56ad
Rogier Lommers <rlommers@Mac.localdomain>
2025-01-08 16:54:21 +0100

    Update resume, add KUBUS

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -11,14 +12,29 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.Use(loggingMiddleware)
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("assets")))
+	router.PathPrefix("/").Handler(http.StripPrefix("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !isValidPath(r.URL.Path) {
+			http.NotFound(w, r)
+			return
+		}
+		http.FileServer(http.Dir("assets")).ServeHTTP(w, r)
+	})))
 
 	cwd, _ := os.Getwd()
 
 	logrus.Infof("serving on http://localhost:8080, cwd: %s", cwd)
 	if err := http.ListenAndServe(":8080", router); err != nil {
-		logrus.Fatal(err)
+		logrus.WithFields(logrus.Fields{
+			"address": ":8080",
+			"router":  router,
+		}).Fatal("Failed to start server: ", err)
 	}
+}
+
+func isValidPath(path string) bool {
+	// Add security checks to ensure the path is within the "assets" directory
+	// For example, you can check for ".." to prevent directory traversal attacks
+	return !strings.Contains(path, "..")
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
